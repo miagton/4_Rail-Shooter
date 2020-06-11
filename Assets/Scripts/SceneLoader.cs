8fadf1661a1376e72de7974491e24e11f9d0bea0
Miagkostup_Igor <justbl0w1991@gmail.com>
2020-06-11 15:11:20 +0300

    LVL0 + LVL created

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] float waitUntilLoad = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadNextScene", waitUntilLoad);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LoadNextScene()
+    {
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = sceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+    }
+}
